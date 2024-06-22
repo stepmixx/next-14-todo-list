17f2595e91a896e220ab9332e7bf3e8f7e03290d
Stephan <stepmix123@gmail.com>
2024-06-22 01:34:47 -0400

    drizzle migrations

---
+++ src/db/migrations/0000_ambitious_rafael_vega.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "todos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"completed" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
